154c5d937d68d6bc6398b53054812abbdee67390
Daniel Michel <65034538+daniel-michel@users.noreply.github.com>
2022-05-18 21:28:56 +0200

    Add Simple AppFrame Test

---
+++ test/screens/app_frame_test.dart
@@ -0,0 +1,28 @@
+import 'package:app/screens/avatar_screen.dart';
+import 'package:app/screens/equip_screen.dart';
+import 'package:app/screens/task_screen.dart';
+import 'package:app/screens/team_screen.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:app/main.dart';
+
+void main() {
+  testWidgets("AppFrame tab selection test", (WidgetTester tester) async {
+
+    await tester.pumpWidget(const App());
+
+    expect(find.byType(TeamScreen), findsOneWidget);
+
+    await tester.tap(find.byTooltip("Tasks"));
+    await tester.pumpAndSettle();
+    expect(find.byType(TaskScreen), findsOneWidget);
+
+    await tester.tap(find.byTooltip("Equip"));
+    await tester.pumpAndSettle();
+    expect(find.byType(EquipScreen), findsOneWidget);
+    
+    await tester.tap(find.byTooltip("Avatar"));
+    await tester.pumpAndSettle();
+    expect(find.byType(AvatarScreen), findsOneWidget);
+  });
+}
