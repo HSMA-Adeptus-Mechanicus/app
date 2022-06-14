059a4af26c17c0641ca7d3c197c1e59053f701ec
Daniel Michel <65034538+daniel-michel@users.noreply.github.com>
2022-06-14 18:08:29 +0200

    Integrate splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sff/screens/login/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -9,24 +8,6 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreen extends State<SplashScreen> {
-  int splashtime = 4;
-  // duration of splash screen on second
-
-  @override
-  void initState() {
-    Future.delayed(Duration(seconds: splashtime), () async {
-      Navigator.pushReplacement(context, MaterialPageRoute(
-          //pushReplacement = replacing the route so that
-          //splash screen won't show on back button press
-          //navigation to Home page.
-          builder: (context) {
-        return blub;
-      }));
-    });
-
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
