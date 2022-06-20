45ed65b484ae31ee785405b78b5bcd7159bd410c
Jan Zimmermann <Janzoo1234@gmail.com>
2022-06-20 16:43:11 +0200

    forgot to add null safety check

--- lib/screens/pages/reward_screen.dart
+++ lib/screens/pages/reward_screen.dart
@@ -13,52 +13,55 @@ class RewardScreen extends StatelessWidget {
         streams: Tuple2(
             UserAuthentication().getUsernameStream(), data.getUsersStream()),
         builder: (context, snapshot) {
-          String username = snapshot.item1.data as String;
-          User user = (snapshot.item2.data as List<User>)
-              .where(
-                  (user) => UserAuthentication.getInstance().userId == user.id)
-              .first;
-          //return Text("Username: $username\nCoins: ${user.currency}");
-          return Padding(
-            padding: EdgeInsets.fromLTRB(
-                13.0, 25.0, 0.0, 0.0), //EdgeInset Top 24.0 but left only 10.0
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Image(
-                        image: Image.asset(
-                          "assets/icons/Pixel/Muenze.PNG",
-                          scale: 1.3,
-                        ).image,
-                      ),
-                      SizedBox(width: 7.0),
-                      Text(
-                        "${user.currency}",
-                        style: TextStyle(
-                          color: Colors.white,
+          if (snapshot.item1.hasData && snapshot.item2.hasData) {
+            String username = snapshot.item1.data as String;
+            User user = (snapshot.item2.data as List<User>)
+                .where((user) =>
+                    UserAuthentication.getInstance().userId == user.id)
+                .first;
+            //return Text("Username: $username\nCoins: ${user.currency}");
+            return Padding(
+              padding: EdgeInsets.fromLTRB(
+                  13.0, 25.0, 0.0, 0.0), //EdgeInset Top 24.0 but left only 10.0
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image(
+                          image: Image.asset(
+                            "assets/icons/Pixel/Muenze.PNG",
+                            scale: 1.3,
+                          ).image,
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 80.0),
-                  Row(
-                    children: [
-                      // 15 sollte keine magic number sein
-                      Text(
-                        " Du hast ${user.currency} Münzen\n gesammelt, ${username}!\n ${user.currency >= 15 ? "Hol dir dein neues Item!" : "Dir fehlen ${15 - user.currency} Münzen\n zum neuen Item!"}",
-                        style: TextStyle(
-                          color: Colors.white,
+                        SizedBox(width: 7.0),
+                        Text(
+                          "${user.currency}",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
-                      )
-                    ],
-                  )
-                ],
+                      ],
+                    ),
+                    SizedBox(height: 80.0),
+                    Row(
+                      children: [
+                        // 15 sollte keine magic number sein
+                        Text(
+                          " Du hast ${user.currency} Münzen\n gesammelt, ${username}!\n ${user.currency >= 15 ? "Hol dir dein neues Item!" : "Dir fehlen ${15 - user.currency} Münzen\n zum neuen Item!"}",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          );
+            );
+          }
+          return CircularProgressIndicator();
         });
   }
 }
